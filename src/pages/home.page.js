c581b9c555dd6b03afec324b3c5d7cd8c8791300
Eric Vazquez <ericjhonatan.vazquez@bbva.com>
2024-06-21 09:23:48 -0600

    se configuro un dropdown en mdc

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -3,6 +3,8 @@ import { BasePage } from "../components/base_components/base-page";
 import "@material/web/button/filled-button.js";
 import "@material/web/button/outlined-button.js";
 import "@material/web/checkbox/checkbox.js";
+import "@material/web/menu/menu.js";
+import "@material/web/menu/menu-item.js";
 
 class HomePage extends BasePage {
   static styles = css`
@@ -16,6 +18,15 @@ class HomePage extends BasePage {
     super();
   }
 
+  firstUpdated() {
+    const anchorEl = this.shadowRoot.querySelector("#usage-anchor");
+    const menuEl = this.shadowRoot.querySelector("#usage-menu");
+
+    anchorEl.addEventListener("click", () => {
+      menuEl.open = !menuEl.open;
+    });
+  }
+
   render() {
     return html`
       <h1>Home Page</h1>
@@ -27,6 +38,21 @@ class HomePage extends BasePage {
 
       <md-outlined-button>Back</md-outlined-button>
       <md-filled-button>Next</md-filled-button>
+      <!-- Note the position: relative style -->
+      <span style="position: relative">
+        <md-filled-button id="usage-anchor">Set with idref</md-filled-button>
+        <md-menu id="usage-menu" anchor="usage-anchor">
+          <md-menu-item>
+            <div slot="headline">Apple</div>
+          </md-menu-item>
+          <md-menu-item>
+            <div slot="headline">Banana</div>
+          </md-menu-item>
+          <md-menu-item>
+            <div slot="headline">Cucumber</div>
+          </md-menu-item>
+        </md-menu>
+      </span>
     `;
   }
 }
